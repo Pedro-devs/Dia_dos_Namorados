4b5d13a138878c74556a55fe749121e5421ae146
Pedro Henrique Andreatti <106180196+Pedro-devs@users.noreply.github.com>
2026-06-12 23:40:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+contar = 0
+function contarBeijos(){
+   resultado = document.getElementById("resultado")
+   contar = contar + 1
+
+   resultado.innerHTML = "Número de beijinhos que você está me devendo : " + contar
+
+
+}
